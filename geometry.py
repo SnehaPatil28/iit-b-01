651dfc9f0f76043df220d749976d45d71f9b9d62
Sneha Patil <snehapatil5581@gmail.com>
2025-12-11 00:32:17 +0530

    geometry function

---
+++ geometry.py
@@ -0,0 +1,5 @@
+def calc_rect_area(length, breadth):
+    print("Area is ",length * breadth)
+
+def calc_rect_perimeter(length, breadth):
+    print("Perimeter is ",2 * (length + breadth))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import arithmetic as ar
+import geometry as geo
 
 print("Hello Everone!!")
 
@@ -7,3 +8,8 @@ b = int(input("Enter second number: "))
 
 ar.sum(a, b)
 ar.subtract(a, b)
+
+length = float(input("Enter length of rectangle: "))
+breadth = float(input("Enter breadth of rectangle: "))
+geo.calc_rect_area(length, breadth)
+geo.calc_rect_perimeter(length, breadth)
