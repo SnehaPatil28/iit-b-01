acc5c327c6cbb049f915a44abffea47030771a68
Sneha Patil <snehapatil5581@gmail.com>
2025-12-11 00:23:39 +0530

    arithmetic function

---
+++ arithmetic.py
@@ -0,0 +1,5 @@
+def sum(a, b):
+    print("Calculating sum: ", a + b)
+
+def subtract(a, b):
+    print("Calculating difference: ", a - b)
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import arithmetic as ar
+
 print("Hello Everone!!")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+ar.sum(a, b)
+ar.subtract(a, b)
